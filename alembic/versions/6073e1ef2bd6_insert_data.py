fbfd117b3b442e046cb0e68c02e8c3eafe9ced8e
vadim <vadimbor93@mail.ru>
2023-06-05 15:57:41 +0300

    поправлены миграции, чтобы правильно генерилось айди user

--- alembic/versions/6073e1ef2bd6_insert_data.py
+++ alembic/versions/6073e1ef2bd6_insert_data.py
@@ -17,7 +17,7 @@ branch_labels = None
 depends_on = None
 
 
-accounts_table = table(
+user_table = table(
     'user',
     column('id', Integer),
     column('username', String),
@@ -27,18 +27,18 @@ accounts_table = table(
 
 def upgrade():
     op.bulk_insert(
-        accounts_table,
+        user_table,
         [
-            {'id': 1, 'username': 'Vadim Volkovsky', 'message_counter': 0},
-            {'id': 2, 'username': 'Minubaev Malik', 'message_counter': 0},
-            {'id': 3, 'username': 'Erik Matiz', 'message_counter': 0},
-            {'id': 4, 'username': 'Mark Lutz', 'message_counter': 0},
-            {'id': 5, 'username': 'Andrey Pronin', 'message_counter': 0},
-            {'id': 6, 'username': 'Kate', 'message_counter': 0},
-            {'id': 7, 'username': 'Khristina', 'message_counter': 0},
-            {'id': 8, 'username': 'Napoleon', 'message_counter': 0},
-            {'id': 9, 'username': 'Egor', 'message_counter': 0},
-            {'id': 10, 'username': 'Dmitry', 'message_counter': 0},
+            {'username': 'Vadim Volkovsky', 'message_counter': 0},
+            {'username': 'Minubaev Malik', 'message_counter': 0},
+            {'username': 'Erik Matiz', 'message_counter': 0},
+            {'username': 'Mark Lutz', 'message_counter': 0},
+            {'username': 'Andrey Pronin', 'message_counter': 0},
+            {'username': 'Kate', 'message_counter': 0},
+            {'username': 'Khristina', 'message_counter': 0},
+            {'username': 'Napoleon', 'message_counter': 0},
+            {'username': 'Egor', 'message_counter': 0},
+            {'username': 'Dmitry', 'message_counter': 0},
         ]
     )
 
